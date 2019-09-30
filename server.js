64fca3c5cd49f4cdd9426ba45d543e70cce4281a
Nenad Zivotic <nenadzivotic94@gmail.com>
2019-09-30 21:52:39 +0200

    heroku

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(favicon('food-recipes-app' + '/build/favicon.ico'));
+app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
-app.use(express.static('food-recipes-app'));
-app.use(express.static(path.join('food-recipes-app', 'build')));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join('food-recipes-app', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
